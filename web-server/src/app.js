cc7eb7c5bee6383ddf3fb8a8931086c23f627ec5
gopovesteban <36285981+gopovesteban@users.noreply.github.com>
2020-06-27 13:41:19 -0600

    commit 2

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,27 +1,77 @@
+const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
+const { response } = require('express')
+
 
 const app = express()
 
+//Define paths fo Express config
+const publicDir = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDir))
+
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.render('index', {
+        title: 'weather app',
+        name: ' Esteban Gonzalez'
+    })
 })
 
-app.get('/help',(req, res) => {
-    res.send('Help page')
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Esteban Gonzalez'
+    })
 })
 
-app.get('/about',(req, res) => {
-    res.send('About the app page')
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help page',
+        content: 'Submit your questions here',
+        name: 'Esteban Gonzalez'
+    })
 })
 
 app.get('/weather',(req, res) => {
-    res.send('Weather page')
+    res.send({
+        location: 'bahamas',
+        weather: 'tropical sunny'
+    })
 })
 
 
 // app.com
 //app.com/help
 //app.com/about
+
+app.get('/help/*',(req, res) =>{
+    res.render('404',
+    {
+        title: '404',
+        name: 'Esteban Gonzalez',
+        errorMessage: 'Help article not found',
+
+    })
+})
+
+app.get('*',(req, res) =>{
+    res.render('404',
+    {
+        title: '404',
+        name: 'Esteban Gonzalez',
+        errorMessage: 'Page not found',
+
+    })
+})
 
 app.listen(3000, ()=>{
     console.log('Server is up on port 3000')
